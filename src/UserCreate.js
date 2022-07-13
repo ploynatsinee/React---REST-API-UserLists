d1161b2cf7a434b402015f7e30ba5490d3430687
Natsinee Tanapornrat <ploy.natsinee@gmail.com>
2022-07-13 12:04:07 +0700

    add type submit

--- src/UserCreate.js
+++ src/UserCreate.js
@@ -1,10 +1,45 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import { Button, DialogContentText, Grid, TextField, Typography } from '@mui/material';
 
 export default function UserCreate() {
+
+  const handleSubmit = event => {
+    var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "fname": fname,
+  "lname": lname,
+  "username": username,
+  "email": email,
+  "avatar": avatar
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://www.mecallapi.com/api/users/create", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    alert(result['message'])
+    if (result['ststus'] === 'ok') {
+      window.location.href = '/'
+    }
+  })
+  .catch(error => console.log('error', error));
+  }
+
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [avatar, setAvatar] = useState('');
   return (
     <React.Fragment>
       <CssBaseline />
@@ -12,25 +47,30 @@ export default function UserCreate() {
       <Typography variant="h6" gutterBottom component="div">
       Create User
       </Typography>
-      <from>
+      <from onSubmit={handleSubmit}>
         <Grid container spacing={2}>
           <Grid item xs={12} >
-          <TextField id="fname" label="First Name" variant="outlined" fullWidth required/>
+          <TextField id="fname" label="First Name" variant="outlined" fullWidth required
+          onChange={(e) => setFname(e.target.value)}/>
           </Grid>
           <Grid item xs={12} >
-          <TextField id="lname" label="Last Name" variant="outlined" fullWidth required/>
+          <TextField id="lname" label="Last Name" variant="outlined" fullWidth required
+          onChange={(e) => setLname(e.target.value)}/>
           </Grid>
           <Grid item xs={12} sm={6}>
-          <TextField id="username" label="Username" variant="outlined" fullWidth required/>
+          <TextField id="username" label="Username" variant="outlined" fullWidth required
+          onChange={(e) => setUsername(e.target.value)}/>
           </Grid>
           <Grid item xs={12} sm={6}>
-          <TextField id="email" label="Email" variant="outlined" fullWidth required/>
+          <TextField id="email" label="Email" variant="outlined" fullWidth required
+          onChange={(e) => setEmail(e.target.value)}/>
           </Grid>
           <Grid item xs={12} >
-          <TextField id="avatar" label="Avatar" variant="outlined" fullWidth required/>
+          <TextField id="avatar" label="Avatar" variant="outlined" fullWidth required
+          onChange={(e) => setAvatar(e.target.value)}/>
           </Grid>
           <Grid item xs={12}>
-          <Button variant="contained" fullWidth>Create</Button>
+          <Button type="submit" variant="contained" fullWidth>Create</Button>
           </Grid>
         </Grid>
       </from>
